b50ce199ab2f1dec1292db46e61977ed4f48d6bf
Adérito Fernandes <aderito@memogadget.com>
2024-01-01 22:13:19 +0000

    aq

--- plugins/cordova-plugin-roger-radio-player_230830/src/android/WSProcessor.java
+++
@@ -1,7 +0,0 @@
-package com.theholyroger.WSProcessor;
-
-import org.json.JSONObject;
-
-public class WSProcessor {
-    public void processWsMsg(JSONObject jsonMsg) {}
-}
--- plugins/cordova-plugin-rogerradio-auto_230829/www/cordova-plugin-rogerradio-auto.js
+++
@@ -1,5 +0,0 @@
-
-/** @class */
-function CordovaPluginRogerradioAuto() {
-  return this;
-}
