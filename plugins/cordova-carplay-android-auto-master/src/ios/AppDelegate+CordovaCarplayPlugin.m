dfec7da6f3da333d97b4427f6cfc9bda522e4317
Adérito Fernandes <aderito@memogadget.com>
2024-01-05 09:42:50 +0000

    plugin1

---
+++ plugins/cordova-carplay-android-auto-master/src/ios/AppDelegate+CordovaCarplayPlugin.m
@@ -0,0 +1,104 @@
+// copyright 2019 by Mike Nelson, beweb
+// open source by MIT license terms
+
+#import "AppDelegate+CordovaCarplayPlugin.h"
+#import "CordovaCarplayPlugin.h"
+#import <objc/runtime.h>
+#import <MediaPlayer/MediaPlayer.h>
+
+// https://developer.apple.com/documentation/mediaplayer/MPPlayableContentManager?language=objc
+MPPlayableContentManager* playableContentManager;
+
+@interface AppDelegate () 
+- (NSInteger)numberOfChildItemsAtIndexPath:(NSIndexPath *)indexPath;
+@end
+
+
+
+#define kApplicationInBackgroundKey @"applicationInBackground"
+#define kDelegateKey @"delegate"
+
+@implementation AppDelegate (CordovaCarplayPlugin)
+
+- (void)setDelegate:(id)delegate {
+    objc_setAssociatedObject(self, kDelegateKey, delegate, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (id)delegate {
+    return objc_getAssociatedObject(self, kDelegateKey);
+}
+
+
+- (void)setApplicationInBackground:(NSNumber *)applicationInBackground {
+    objc_setAssociatedObject(self, kApplicationInBackgroundKey, applicationInBackground, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (NSNumber *)applicationInBackground {
+    return objc_getAssociatedObject(self, kApplicationInBackgroundKey);
+}
+
+- (BOOL)application:(UIApplication *)application swizzledDidFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [self application:application swizzledDidFinishLaunchingWithOptions:launchOptions];
+    
+ //   @try{
+
+   //     MPPlayableContentManager *contentManager = [MPPlayableContentManager sharedContentManager];
+  //      contentManager.dataSource = self;
+   //     contentManager.delegate = self;
+      
+    //    [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
+        
+        self.applicationInBackground = @(YES);
+        
+//    }@catch (NSException *exception) {
+  //  }
+
+    return YES;
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    self.applicationInBackground = @(NO);
+    
+    //endReceivingRemoteControlEvents and resignFirstResponder
+    NSLog(@"carplay plugin applicationDidBecomeActive - not doing anything here");
+    
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    self.applicationInBackground = @(YES);
+    NSLog(@"entered background");
+}
+
+
+//Tells the app that a remote notification arrived that indicates there is data to be fetched.
+// Called when a message arrives in the foreground and remote notifications permission has been granted
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+    fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+
+    @try{
+        NSDictionary *mutableUserInfo = [userInfo mutableCopy];
+        NSDictionary* aps = [mutableUserInfo objectForKey:@"aps"];
+        if([aps objectForKey:@"alert"] != nil){
+            [mutableUserInfo setValue:@"notification" forKey:@"messageType"];
+            NSString* tap;
+            if([self.applicationInBackground isEqual:[NSNumber numberWithBool:YES]]){
+                tap = @"background";
+            }
+            [mutableUserInfo setValue:tap forKey:@"tap"];
+        }else{
+            [mutableUserInfo setValue:@"data" forKey:@"messageType"];
+        }
+
+        NSLog(@"didReceiveRemoteNotification: %@", mutableUserInfo);
+        
+        completionHandler(UIBackgroundFetchResultNewData);
+     //   [FirebasePlugin.firebasePlugin sendNotification:mutableUserInfo];
+    }@catch (NSException *exception) {
+   //     [FirebasePlugin.firebasePlugin handlePluginExceptionWithoutContext:exception];
+    }
+}
+
+
+
+
+@end
---
+++ plugins/cordova-carplay-android-auto-master/www/plugin-carplay-android-auto.js
@@ -0,0 +1,59 @@
+
+/** @class */
+function CarplayAndroidAutoPlugin() {
+  return this;
+}
+
+CarplayAndroidAutoPlugin.prototype.setMediaItems = function(mediaItems) {
+  var options = JSON.stringify(mediaItems);
+  cordova.exec(function(){}, function(){}, 'CordovaCarplayPlugin', 'setMediaItems', [options]);
+}
+
+CarplayAndroidAutoPlugin.prototype.removeMediaItems = function(indexPathPrefix) {
+  cordova.exec(function(){}, function(){}, 'CordovaCarplayPlugin', 'removeMediaItems', [indexPathPrefix]);
+}
+
+CarplayAndroidAutoPlugin.prototype.registerHandler = function(successCallback, errorCallback) {
+  var options = {};
+  cordova.exec(successCallback, errorCallback, 'CordovaCarplayPlugin', 'registerHandler', [options]);
+}
+
+/**
+ * @param {string} mediaItemID
+ * @param {string} title
+ * @param {string} subtitle
+ * @param {string} album
+ * @param {string} coverImage
+ * @param {number} duration
+ * @param {number} elapsed
+ * @param {number} queueIndex
+ * @param {number} queueCount
+ * @param {number} playButtonState
+ */
+CarplayAndroidAutoPlugin.prototype.updateNowPlayingMetaData = function(mediaItemID, title, subtitle, album, coverImage, duration, elapsed, queueIndex, queueCount, playButtonState) {
+  cordova.exec(function(){}, function(){}, 'CordovaCarplayPlugin', 'updateNowPlayingMetaData', [title, subtitle, album, coverImage, duration || 0, elapsed || 0, mediaItemID, queueIndex ||0, queueCount||0, playButtonState||0]);
+}
+
+CarplayAndroidAutoPlugin.prototype.setCommandEnabled = function(commandName, isEnabled) {
+	cordova.exec(function(){}, function(){}, 'CordovaCarplayPlugin', 'setCommandEnabled', [commandName, isEnabled]);
+}
+
+CarplayAndroidAutoPlugin.prototype.finishedPlaying = function() {
+  var options = {};
+  // a little delay as carplay sometimes show blank now playing otherwise
+  window.setTimeout(function(){
+    cordova.exec(function(){}, function(){}, 'CordovaCarplayPlugin', 'finishedPlaying', [options]);
+  }, 100);
+}
+
+
+// Installation constructor that binds the plugin to window
+CarplayAndroidAutoPlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.carplayAndroidAutoPlugin = new CarplayAndroidAutoPlugin();
+  return window.plugins.carplayAndroidAutoPlugin;
+};
+cordova.addConstructor(CarplayAndroidAutoPlugin.install);
+
